13e96f97796578493b6e3d7e795fae79d6afb37f
Ayush <raj07ayush@gmail.com>
2026-05-05 19:39:06 +0530

    requirements changes

--- app.py
+++ app.py
@@ -92,7 +92,11 @@ ctx = webrtc_streamer(
     rtc_configuration={
         "iceServers": [
             {
-                "urls": ["stun:stun.l.google.com:19302"]
+                "urls": [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302"
+                ]
             }
         ]
     },
